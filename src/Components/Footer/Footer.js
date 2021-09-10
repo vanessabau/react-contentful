94885350309ec38827332bd97d98c194739d9f1c
Vanessa Bautista <vanessawebdev@gmail.com>
2021-09-10 08:45:34 -0700

    initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer class="footer mt-auto py-3">
+        <div class="container">
+          <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
